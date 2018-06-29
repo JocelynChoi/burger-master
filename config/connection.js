fd27b2444e16cd896a2ae45063b459d76ec45ff4
Jocelyn Choi <choi.jocelyn1000@gmail.com>
2018-06-29 19:30:50 -0400

    init

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,29 @@
 var mysql = require("mysql");
-
-var dbconfig = process.env.JAWSDB_URL || {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Suanmokk54!$",
-  database: "burgers_db"
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Suanmokk54!$",
+    database: "burgers_db"
+  });
 };
 
-var connection = mysql.createConnection(dbconfig);
+// var dbconfig = process.env.JAWSDB_URL || {
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Suanmokk54!$",
+//   database: "burgers_db"
+// };
+
+// var connection = mysql.createConnection(dbconfig);
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
